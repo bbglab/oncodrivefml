c4a36f429bf45a40bfd4626da8c7a9bda0302699
I. Reyes-Salazar <iker.reyes@irbbarcelona.org>
2017-03-14 16:54:05 +0100

    Removed old workaround

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,9 +21,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-# Local modifications for the conda environment
-sys.path.insert(0, '/home/iker/anaconda3/envs/macao_oncodrive/lib/python3.5/site-packages/tabix')
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
