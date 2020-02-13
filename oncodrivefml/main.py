f41c786b460ee924c3f42f6e666833b53fbd654c
I. Reyes <iker.reyes@irbbarcelona.org>
2020-02-13 15:39:42 +0100

    Deprecated sequencing option an replaced by signature option

--- oncodrivefml/main.py
+++ oncodrivefml/main.py
@@ -31,8 +31,6 @@ def main(mutations_file, elements_file, output_folder, config_file, samples_blac
         config_file (str): path to configuration file
         samples_blacklist (str): path to samples blacklist file. Set to :obj:`None`
            if you are not using any blacklist file
-        seed (int): initial random seed
-        generate_pickle (bool): whether run OncodriveFML to generate pickle files or full analysis
         config_override_dict (dict, optional): override configuration from file
 
     """
@@ -63,11 +61,12 @@ def main(mutations_file, elements_file, output_folder, config_file, samples_blac
 @click.option('-i', '--input', 'mutations_file', type=click.Path(exists=True), help='Variants file', metavar='MUTATIONS_FILE', required=True)
 @click.option('-e', '--elements', 'elements_file', type=click.Path(exists=True), metavar='ELEMENTS_FILE', help='Genomic elements to analyse', required=True)
 @click.option('-t', '--type', type=click.Choice(['coding', 'noncoding']), help='Deprecated option')
-@click.option('-s', '--sequencing', type=click.Choice(['wgs', 'wes', 'targeted']), help='Type of sequencing: whole genome, whole exome or targeted.', required=True)
+@click.option('-s', '--sequencing', type=click.Choice(['wgs', 'wes', 'targeted']), help='Type of sequencing: whole genome, whole exome or targeted.')
 @click.option('-o', '--output', 'output_folder', type=click.Path(), metavar='OUTPUT_FOLDER', help="Output folder. Default to regions file name without extensions.", default=None)
 @click.option('-c', '--configuration', 'config_file', default=None, type=click.Path(exists=True), metavar='CONFIG_FILE', help="Configuration file. Default to 'oncodrivefml_v2.conf' in the current folder if exists or to ~/.config/bbglab/oncodrivefml_v2.conf if not.")
 @click.option('--samples-blacklist', default=None, type=click.Path(exists=True), metavar='SAMPLES_BLACKLIST', help="Remove these samples when loading the input file.")
 @click.option('--signature', 'signature_file', default=None, type=click.Path(exists=True), metavar='SIGNATURE', help="File with the signatures to use")
+@click.option('--signature-correction', type=click.Choice(['wg', 'wx']), help='Correct the computed signutares by genomic or exomic signtures. Only valid for human genomes', default=None)
 @click.option('--no-indels', help="Discard indels in your analysis", is_flag=True)
 @click.option('--cores', help="Cores to use. Default: all", default=None, type=int)
 @click.option('--seed', help="Set up an initial random seed to have reproducible results", type=click.IntRange(0, 2**32-1), default=None)
@@ -75,7 +74,7 @@ def main(mutations_file, elements_file, output_folder, config_file, samples_blac
 @click.option('--debug', help="Show more progress details", is_flag=True)
 @click.version_option(version=__version__)
 def cmdline(mutations_file, elements_file, type, sequencing, output_folder, config_file, samples_blacklist,
-            signature_file, no_indels, cores, seed, generate_pickle, debug):
+            signature_file, signature_correction, no_indels, cores, seed, generate_pickle, debug):
     """
     Run OncodriveFML on the genomic regions in ELEMENTS FILE
     using the mutations in MUTATIONS FILE.
@@ -99,9 +98,14 @@ def cmdline(mutations_file, elements_file, type, sequencing, output_folder, conf
                       'Edit the configuration file appropriately',
                       DeprecationWarning)
 
-    if sequencing == 'wes':
+    if sequencing is not None:
+        warnings.warn('--sequencing option has been replaced by '
+                      '--signature-correction',
+                      DeprecationWarning)
+
+    if signature_correction == 'wx' or (signature_correction is None and sequencing == 'wes'):
         override_config['signature']['normalize_by_sites'] = 'whole_exome'
-    elif sequencing == 'wgs':
+    elif signature_correction == 'wg' or (signature_correction is None and sequencing == 'wgs'):
         override_config['signature']['normalize_by_sites'] = 'whole_genome'
     else:
         override_config['signature']['normalize_by_sites'] = None
